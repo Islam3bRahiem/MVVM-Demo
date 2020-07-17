246455f63e0deb6140e892d85b57927cb2a4cffc
Islam_3brahiem <islam.rahiem94@gmail.com>
2020-07-17 11:11:19 +0200

    Initial Commit

---
+++ MVVM-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Demo
+//
+//  Created by Islam 3bRahiem on 7/17/20.
+//  Copyright © 2020 Organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
